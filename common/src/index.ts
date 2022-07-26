54bee1603e4dd5ae59d1bfe2029493da8cec9701
yichu lau <yichu21@gmail.com>
2022-07-26 14:59:02 +0800

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,3 +1,4 @@
+// Re-export stuff from errors and middlewares
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
@@ -9,4 +10,3 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
-
