8167b4c50b2cc11d0b931277efabea4273dcd6f3
yichu lau <yichu21@gmail.com>
2022-07-29 10:12:12 +0800

    Updates

---
+++ common/src/events/types/order-status.ts
@@ -0,0 +1,13 @@
+export enum OrderStatus {
+    // when the order has been created, but the ticket it is trying to order has not been reserved
+    Created = 'created',
+
+    // the ticket the order is trying to reserve has already been reserved, or when the user has cancelled the order
+    Cancelled = 'cancelled',
+
+    // the order has successfully reserved the ticket
+    AwaitingPayment = 'awaiting:payment',
+
+    // complete and provided payment successfully
+    Complete = 'complete'
+}
