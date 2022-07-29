1f029498b2d42c20fb0f35ce2460e905cbb9cd81
yichu lau <yichu21@gmail.com>
2022-07-29 13:57:04 +0800

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -2,4 +2,6 @@ export enum Subjects{
     TicketCreated = 'ticket:created',
     OrderUpdated = 'order:updated',
     TicketUpdated = 'ticket:updated',
+    OrderCreated = 'order:created',
+    OrderCancelled = 'order:cancelled',
 }
